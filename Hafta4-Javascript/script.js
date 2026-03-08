10e176449cade64c94b3d8e88ab3f47c81a7519b
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2026-03-09 01:53:49 +0300

    Javascript basics

---
+++ Hafta4-Javascript/script.js
@@ -0,0 +1,53 @@
+// Javascript nedir?
+// 1995 yılı
+// Brendan Eich
+// Netscape
+// Mozilla 
+// Javascript motorları
+
+// Hello world
+
+// Kullanıcıdan veri alma
+
+// Değişken tanımlama
+    // var
+    // let
+    // const
+
+// Operatörler
+    // + toplama 	
+    // - çıkarma
+    // * çarpma
+    // ** üs alma
+    // / bölme
+    // % kalan
+    // ++ artırma
+    // -- azaltma
+
+    // == ve === farkı
+    // != ve !==
+    // >
+    // <
+    // >=
+    // <=
+
+//Koşullar(Conditions)
+
+
+//Döngüler (Loops)
+    // for
+    // while
+    // do while
+
+
+// Functions
+
+// Arrow funct
+
+// Parametreli ve parametresiz fonksiyon
+
+// Default parametre
+
+// Objects
+
+// Object Constructor
